f9a48d96dffdbde8c8a32f20f673e331de723e96
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 12:31:50 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,37 +1,19 @@
 def second_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
-        },
-      :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
-        },
-      :hero => {
-        :name => "Romeo",
-        :age => "15",
-        :status => "alive"
-        },
+    :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
-   }, 
-   :capulet => {
-      :patriarch => {
-        :name => "Lord Capulet",
-        :age => "50"
-        },
-      :matriarch => {
-        :name => "Lady Capulet",
-        :age => "51"
-        },
-      :heroine => {
-        :name => "Juliet",
-        :age => "15",
-        :status => "alive"
-        },
-      :heroine_friends => []
-   }
+      }, 
+    :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :hero_friends => []
+      }
+    
   }
 
-  end
+  
+end
